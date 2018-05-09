a29f4ba57ef0044eb2826ac4cfa11012d896c158
Robert Jackson <rjackson@linkedin.com>
2018-05-08 22:42:09 -0700

    Add test for parse + modify + print.

--- tests/index-test.js
+++ tests/index-test.js
@@ -6,10 +6,26 @@ QUnit.module('ember-template-recast', function() {
     let template = stripIndent`
       {{foo-bar
         baz="stuff"
-      }}
-    `;
+      }}`;
     let ast = parse(template);
 
     assert.equal(print(ast), template);
+  });
+
+  QUnit.test('basic parse -> mutation -> print', function(assert) {
+    let template = stripIndent`
+      {{foo-bar
+        baz="stuff"
+      }}`;
+    let ast = parse(template);
+    ast.body[0].hash.pairs[0].key = 'derp';
+
+    assert.equal(
+      print(ast),
+      stripIndent`
+        {{foo-bar
+          derp="stuff"
+        }}`
+    );
   });
 });
