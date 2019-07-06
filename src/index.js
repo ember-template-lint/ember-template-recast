744f2a2d680a0444b9a26e503b6074d84dc06dd9
Robert Jackson <rjackson@linkedin.com>
2019-07-06 01:55:24 -0400

    Remove "modification" tracking code in favor of always reprinting.

--- src/index.js
+++ src/index.js
@@ -8,46 +8,69 @@ const {
 
 const reLines = /(.*?(?:\r\n?|\n|$))/gm;
 
-const PARSE_RESULT = Symbol('PARSE_RESULT');
-const NEAREST_NODE_WITH_LOC = Symbol('NEAREST_NODE_WITH_LOC');
-const CLONED_NEAREST_NODE_WITH_LOC = Symbol('CLONED_NEAREST_NODE_WITH_LOC');
+const PARSE_RESULT_FOR = new WeakMap();
+const SOURCE_FOR = new WeakMap();
+const ANCESTOR_FOR = new WeakMap();
+const DIRTY_FIELDS_FOR = new WeakMap();
 
-function _isSynthetic(node) {
-  if (node && node.loc) {
-    return node.loc.source === '(synthetic)';
-  }
-  return true;
-}
-
-function linesFrom(string) {
-  // split the strip up by \n or \r\n
-  let lines = string.match(reLines);
-
-  // always insist on returning one line (since we always modify one line)
-  // otherwise, lines might disappear!
-  // (if it's whitespace-only after splicing in changes, we'll edit it out below)
-  if (lines.length === 1) {
-    return lines;
+function sourceForNode(node, sourceLines) {
+  if (!node.loc) {
+    return;
   }
 
-  // the split above always results in an extra empty line at the end remove it
-  return lines.slice(0, -1);
+  let firstLine = node.loc.start.line - 1;
+  let lastLine = node.loc.end.line - 1;
+  let currentLine = firstLine - 1;
+  let firstColumn = node.loc.start.column;
+  let lastColumn = node.loc.end.column;
+  let string = [];
+  let line;
+
+  while (currentLine < lastLine) {
+    currentLine++;
+    line = sourceLines[currentLine];
+
+    if (currentLine === firstLine) {
+      if (firstLine === lastLine) {
+        string.push(line.slice(firstColumn, lastColumn));
+      } else {
+        string.push(line.slice(firstColumn));
+      }
+    } else if (currentLine === lastLine) {
+      string.push(line.slice(0, lastColumn));
+    } else {
+      string.push(line);
+    }
+  }
+
+  return string.join('');
 }
 
-function wrapNode(node, parentNode, nearestNodeWithLoc, nearestNodeWithStableLoc, parseResult) {
+function markAsDirty(node, property) {
+  let dirtyFields = DIRTY_FIELDS_FOR.get(node);
+  if (dirtyFields === undefined) {
+    dirtyFields = new Set();
+    DIRTY_FIELDS_FOR.set(node, dirtyFields);
+  }
+
+  dirtyFields.add(property);
+
+  let ancestor = ANCESTOR_FOR.get(node);
+  while (ancestor !== null) {
+    markAsDirty(ancestor.node, ancestor.key);
+    ancestor = ANCESTOR_FOR.get(ancestor.node);
+  }
+}
+
+function wrapNode(ancestor, node, sourceLines) {
+  ANCESTOR_FOR.set(node, ancestor);
+  SOURCE_FOR.set(node, sourceForNode(node, sourceLines));
+
+  let hasLocInfo = !!node.loc;
   let propertyProxyMap = new Map();
-  let clonedNearestNodeWithLoc = JSON.parse(JSON.stringify(nearestNodeWithLoc));
 
   let proxy = new Proxy(node, {
     get(target, property) {
-      if (property === NEAREST_NODE_WITH_LOC) {
-        return nearestNodeWithLoc;
-      } else if (property === CLONED_NEAREST_NODE_WITH_LOC) {
-        return clonedNearestNodeWithLoc;
-      } else if (property === PARSE_RESULT) {
-        return parseResult;
-      }
-
       if (propertyProxyMap.has(property)) {
         return propertyProxyMap.get(property);
       }
@@ -56,93 +79,12 @@ function wrapNode(node, parentNode, nearestNodeWithLoc, nearestNodeWithStableLoc
     },
 
     set(target, property, value) {
-      let original = clonedNearestNodeWithLoc;
-      let updatedValue = nearestNodeWithLoc;
-
-      if (propertyProxyMap.has(property)) {
-        let propertyProxy = propertyProxyMap.get(property);
-        original = propertyProxy[CLONED_NEAREST_NODE_WITH_LOC];
-        updatedValue = propertyProxy[NEAREST_NODE_WITH_LOC];
-
-        if (updatedValue === Reflect.get(target, property)) {
-          updatedValue = value;
-        }
-      }
-
       Reflect.set(target, property, value);
 
-      if (Array.isArray(target) && property === 'length') return true;
-
-      if (node.type === 'ElementNode' && property === 'tag') {
-        // change the opening tag name
-        parseResult.addModification({
-          start: {
-            line: original.loc.start.line,
-            column: original.loc.start.column + 1,
-          },
-          end: {
-            line: original.loc.start.line,
-            column: original.loc.start.column + 1 + original.tag.length,
-          },
-          value,
-        });
-
-        if (!node.selfClosing) {
-          // change the closing tag name
-          parseResult.addModification({
-            start: {
-              line: node.loc.end.line,
-              column: node.loc.end.column - 1 - original.tag.length,
-            },
-            end: {
-              line: node.loc.end.line,
-              column: node.loc.end.column - 1,
-            },
-            value,
-          });
-        }
-      } else if (
-        property === 'hash' &&
-        (node.type === 'BlockStatement' || node.type === 'MustacheStatement') &&
-        _isSynthetic(original)
-      ) {
-        // Catches case where we try to replace an empty hash with a hash
-        // that contains entries.
-        const endOfPath = node.path.loc.end;
-        parseResult.addModification({
-          start: endOfPath,
-          end: endOfPath,
-          value: ` ${_print(updatedValue, { entityEncoding: 'raw' })}`,
-        });
-      } else if (Array.isArray(node) && parentNode.type === 'Hash' && _isSynthetic(parentNode)) {
-        // Catches case where we try to push a new hash pair on to a hash
-        // that doesn't contain any entries.
-        const endOfPath = nearestNodeWithStableLoc.path.loc.end;
-        parseResult.addModification({
-          start: endOfPath,
-          end: endOfPath,
-          value: ` ${_print(updatedValue, { entityEncoding: 'raw' })}`,
-        });
+      if (hasLocInfo) {
+        markAsDirty(node, property);
       } else {
-        parseResult.addModification({
-          start: original.loc.start,
-          end: original.loc.end,
-          value: updatedValue,
-        });
-      }
-
-      if (property === 'path' && node.type === 'BlockStatement') {
-        parseResult.addModification({
-          start: {
-            line: node.loc.end.line,
-            column: node.loc.end.column - 2 - original.original.length,
-          },
-          end: {
-            line: node.loc.end.line,
-            column: node.loc.end.column - 2,
-          },
-          value: updatedValue,
-        });
+        markAsDirty(ancestor.node, ancestor.key);
       }
 
       return true;
@@ -152,13 +94,8 @@ function wrapNode(node, parentNode, nearestNodeWithLoc, nearestNodeWithStableLoc
   for (let key in node) {
     let value = node[key];
     if (typeof value === 'object' && value !== null) {
-      let propertyProxy = wrapNode(
-        value,
-        node,
-        value.loc ? value : node.loc ? node : nearestNodeWithLoc,
-        !_isSynthetic(value) ? value : !_isSynthetic(node) ? node : nearestNodeWithStableLoc,
-        parseResult
-      );
+      let propertyProxy = wrapNode({ node, key }, value, sourceLines);
+
       propertyProxyMap.set(key, propertyProxy);
     }
   }
@@ -166,110 +103,72 @@ function wrapNode(node, parentNode, nearestNodeWithLoc, nearestNodeWithStableLoc
   return proxy;
 }
 
-function hasSameLocation(a, b) {
-  return (
-    a.start.line === b.start.line &&
-    a.start.column === b.start.column &&
-    a.end.line === b.end.line &&
-    a.end.column === b.end.column
-  );
-}
-
 class ParseResult {
   constructor(template) {
-    this.source = linesFrom(template);
-    this.modifications = [];
-
     let ast = preprocess(template, {
       mode: 'codemod',
       parseOptions: {
         ignoreStandalone: true,
       },
     });
-    this.ast = wrapNode(ast, null, ast, ast, this);
-  }
 
-  addModification(modification) {
-    let existingModificationIndex = this.modifications.findIndex(m =>
-      hasSameLocation(m, modification)
-    );
-
-    if (existingModificationIndex === -1) {
-      this.modifications.push(modification);
-    } else {
-      this.modifications.splice(existingModificationIndex, 1, modification);
-    }
-  }
-
-  applyModifications() {
-    let { modifications } = this;
-    this.modifications = Object.freeze([]);
-
-    let sortedModifications = modifications
-      .sort((a, b) => a.start.line - b.start.line || a.start.column - b.start.column)
-      .reverse();
-
-    sortedModifications.forEach(({ start, end, value }) => {
-      let printed = typeof value === 'string' ? value : _print(value, { entityEncoding: 'raw' });
-      let firstIndexToUpdate = start.line - 1;
-      let lastIndexToUpdate = end.line - 1;
-      let firstLineContents = this.source[firstIndexToUpdate];
-      let lastLineContents = this.source[lastIndexToUpdate];
-      let replacementLines = linesFrom(printed);
-
-      let mergedReplacementLines = replacementLines.map((line, index) => {
-        let isFirstLine = index === 0;
-        let isLastLine = index === replacementLines.length - 1;
-        let updatedLine = line;
-
-        // We check for a couple of common error cases that can be introduced by content removal...
-        // 1) For one-line edits, if there is whitespace before and after the edited section,
-        //    and if the new content is empty, trim the preceeding whitespace before the edit.
-        //    Also checks for if the only thing following the section is the end of a hbs tag ("}}"),
-        //    but only if the replacement content is empty (just removing stuff).
-        if (isFirstLine && isLastLine && firstLineContents && lastLineContents) {
-          const startSlice = firstLineContents.slice(0, start.column);
-          const endSlice = lastLineContents.slice(end.column);
-          const hasPreceedingWhitespace = startSlice.match(/\S+\s+$/);
-          const hasTrailingWhitespace = endSlice.match(/^\s+\S+/) || endSlice.match(/^}}/);
-
-          if (line === '' && hasPreceedingWhitespace && hasTrailingWhitespace) {
-            // trimEnd() is as of Node 10, so we probably want to use replace() until Node < 10 is EOLd.
-            updatedLine = firstLineContents.slice(0, start.column).replace(/\s+$/, '') + line;
-          } else {
-            updatedLine = firstLineContents.slice(0, start.column) + line;
-          }
-          updatedLine += lastLineContents.slice(end.column);
-        } else {
-          if (isFirstLine && firstLineContents) {
-            updatedLine = firstLineContents.slice(0, start.column) + line;
-          }
-          if (isLastLine && lastLineContents) {
-            updatedLine += lastLineContents.slice(end.column);
-          }
-        }
-
-        // 2) If the only thing that's left on this line is whitespace, and we made a change, remove it.
-        if (updatedLine.match(/\S/) === null && line !== updatedLine) {
-          return null;
-        }
-
-        return updatedLine;
-      });
-
-      this.source.splice(
-        firstIndexToUpdate,
-        1 /* always replace at least one line */ + lastIndexToUpdate - firstIndexToUpdate,
-        ...mergedReplacementLines
-      );
-    });
+    this.source = template.match(reLines);
+    this._originalAst = ast;
+    this.ast = wrapNode(null, ast, this.source);
+    PARSE_RESULT_FOR.set(this.ast, this);
   }
 
   print() {
-    this.applyModifications();
-
-    return this.source.join('');
+    return build(this._originalAst);
   }
+}
+
+function build(ast) {
+  if (!ast) {
+    return '';
+  }
+
+  let source = SOURCE_FOR.get(ast);
+  let dirtyFields = DIRTY_FIELDS_FOR.get(ast) || new Set();
+  if (dirtyFields.size === 0 && source !== undefined) {
+    return source;
+  }
+
+  if (source === undefined) {
+    return _print(ast, { entityEncoding: 'raw' });
+  }
+
+  // TODO: splice the original source **excluding** "children"
+  // based on dirtyFields
+  const output = [];
+
+  switch (ast.type) {
+    case 'Program':
+    case 'Block':
+    case 'Template':
+    case 'ElementNode':
+    case 'AttrNode':
+    case 'ConcatStatement':
+    case 'TextNode':
+    case 'MustacheStatement':
+    case 'MustacheCommentStatement':
+    case 'ElementModifierStatement':
+    case 'PathExpression':
+    case 'SubExpression':
+    case 'BlockStatement':
+    case 'PartialStatement':
+    case 'CommentStatement':
+    case 'Hash':
+    case 'HashPair':
+      break;
+    case 'StringLiteral':
+    case 'NumberLiteral':
+    case 'UndefinedLiteral':
+    case 'NullLiteral':
+    case 'BooleanLiteral':
+  }
+
+  return output.join('');
 }
 
 function parse(template) {
@@ -277,7 +176,7 @@ function parse(template) {
 }
 
 function print(ast) {
-  let parseResult = ast[PARSE_RESULT];
+  let parseResult = PARSE_RESULT_FOR.get(ast);
   return parseResult.print();
 }
 
