bd697f893c592df3c5cb46591c5faf832138fa1e
Robert Jackson <rjackson@linkedin.com>
2019-07-08 00:46:59 -0400

    Cleanup skipped placeholders
    Move the BlockStatement ones to the new BlockStatement QUnit.module,
    remove placeholders that are already added as "real" tests.

--- tests/index-test.js
+++ tests/index-test.js
@@ -728,7 +728,8 @@ QUnit.module('ember-template-recast', function() {
     QUnit.skip('add block param');
     QUnit.skip('remove block param');
     QUnit.skip('add child to inverse with whitespace control');
-    QUnit.skip('{{else if foo}} chaining');
+    QUnit.skip('add {{else if foo}} chaining');
+    QUnit.skip('removing an {{else if foo}} condition');
   });
 
   QUnit.module('HashPair', function() {
@@ -787,15 +788,7 @@ QUnit.module('ember-template-recast', function() {
     assert.equal(code, 'hello world\n{{other-stuff}}');
   });
 
-  QUnit.skip('supports removing an {{else if foo}} condition');
-  QUnit.skip('supports adding a block param');
-  QUnit.skip('supports removing a block param');
   QUnit.skip('supports updating the various literal types');
-  QUnit.skip('supports changing element node tagname and adding attributes');
-  QUnit.skip('preserves whitespace on mustache statement end -> hash mutation');
-  QUnit.skip('preserves whitespace on mustache statement end -> params mutation');
-  QUnit.skip('preserves whitespace on mustache statement end -> path mutation');
-  QUnit.skip('update path on ambiguous mustache statement');
 });
 
 QUnit.module('transform', () => {
