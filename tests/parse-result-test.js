9e06698c483209583651ae410faea884422d6bfc
Timothy Lindvall <tlindvall@linkedin.com>
2019-10-14 15:36:27 -0700

    Handle attr change from text node to mustache
    Fixes GH #139.

--- tests/parse-result-test.js
+++ tests/parse-result-test.js
@@ -52,6 +52,46 @@ QUnit.module('ember-template-recast', function() {
       assert.equal(print(ast), `<FooBar @thing="static thing 1" @baz="static thing 2" />`);
     });
 
+    QUnit.test('changing an attribute value from text node to mustache (GH #139)', function(assert) {
+      let template = `<FooBar @foo="Hi, I'm a string!" />`;
+
+      let ast = parse(template);
+      ast.body[0].attributes[0].value = builders.mustache('my-awesome-helper', [
+        builders.string("hello"),
+        builders.string("world")
+      ]);
+
+      assert.equal(print(ast), `<FooBar @foo={{my-awesome-helper "hello" "world"}} />`);
+    });
+
+    QUnit.test('changing an attribute value from text node to concat statement (GH #139)', function(assert) {
+      let template = `<FooBar @foo="Hi, I'm a string!" />`;
+
+      let ast = parse(template);
+      ast.body[0].attributes[0].value = builders.concat([
+        builders.text("Hello "),
+        builders.mustache('my-awesome-helper', [
+          builders.string("hello"),
+          builders.string("world")
+        ]),
+        builders.text(" world")
+      ]);
+
+      assert.equal(print(ast), `<FooBar @foo="Hello {{my-awesome-helper "hello" "world"}} world" />`);
+    });
+
+    QUnit.test('changing an attribute value from mustache to mustache', function(assert) {
+      let template = `<FooBar @foo={{12345}} />`;
+
+      let ast = parse(template);
+      ast.body[0].attributes[0].value = builders.mustache('my-awesome-helper', [
+        builders.string("hello"),
+        builders.string("world")
+      ]);
+
+      assert.equal(print(ast), `<FooBar @foo={{my-awesome-helper "hello" "world"}} />`);
+    });
+
     QUnit.test('rename element tagname', function(assert) {
       let template = stripIndent`
       <div data-foo='single quoted'>
