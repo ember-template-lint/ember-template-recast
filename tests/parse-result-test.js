ember-template-lint/ember-template-recast

commit a589561e5ac8caf62ce31c25e4bee4b5cd2727bf

Make AttrNode responsible for custom quoting requirements.
The location information (as of @glimmer/syntax@0.47.9) for a `TextNode`
that is the sole value of an `AttrNode` _includes_ any surrounding
quotes. This should be fixed upstream, such that calculating the source
for the `TextNode` accounts for the actual text content (not the
surrounding formatting).

The primary changes here are ensuring that the only thing "aware" of
quotedness (or lack thereof) of an `AttrNode` value _is_ the `AttrNode`.
This is accomplished by adding an additional check to the AST
fixup prepass that we already do. This new fixup updates the location
information when a `TextNode` is an `AttrNode`'s `.value`.

In addition, this commit takes advantage of new capabilities in
`@glimmer/syntax` (passing the `path` in addition to the `node` while
visiting the AST nodes).

src/parse-result.js
@@ -3,7 +3,7 @@ const { sortByLoc, sourceForLoc } = require('./utils');
 
 const reLines = /(.*?(?:\r\n?|\n|$))/gm;
 const leadingWhitespace = /(^\s+)/;
-const attrNodeParts = /(^[^=]+)(\s+)?(=)?(\s+)?(\S+)?/;
+const attrNodeParts = /(^[^=]+)(\s+)?(=)?(\s+)?(['"])?(\S+)?/;
 const hashPairParts = /(^[^=]+)(\s+)?=(\s+)?(\S+)/;
 
 const voidTagNames = new Set([
@@ -52,13 +52,28 @@ function fixASTIssues(sourceLines, ast) {
         node.loc.end.column = node.loc.start.column + node.name.length;
       }
     },
-    ConcatStatement(node) {
-      // TODO: manually working around https://github.com/glimmerjs/glimmer-vm/pull/954
-      if (
-        node.parts[0].type === 'TextNode' &&
-        node.parts[0].loc.start.column > node.loc.start.column + 1
-      ) {
-        node.parts[0].loc.start.column = node.parts[0].loc.start.column - 1;
+    TextNode(node, path) {
+      let source = sourceForLoc(sourceLines, node.loc);
+      switch (path.parentNode.type) {
+        case 'AttrNode': {
+          if (
+            node.chars.length > 0 &&
+            ((source.startsWith(`'`) && source.endsWith(`'`)) ||
+              (source.startsWith(`"`) && source.endsWith(`"`)))
+          ) {
+            node.loc.end.column = node.loc.end.column - 1;
+            node.loc.start.column = node.loc.start.column + 1;
+          }
+          break;
+        }
+        case 'ConcatStatement': {
+          // TODO: manually working around https://github.com/glimmerjs/glimmer-vm/pull/954
+          let isFirstPart = path.parentNode.parts.indexOf(node) === 0;
+
+          if (isFirstPart && node.loc.start.column > path.parentNode.loc.start.column + 1) {
+            node.loc.start.column = node.loc.start.column - 1;
+          }
+        }
       }
     },
   });
@@ -87,11 +102,13 @@ module.exports = class ParseResult {
 
   wrapNode(ancestor, node) {
     this.ancestor.set(node, ancestor);
+
     let nodeInfo = {
       node,
       original: JSON.parse(JSON.stringify(node)),
       source: this.sourceForLoc(node.loc),
     };
+
     this.nodeInfo.set(node, nodeInfo);
 
     let hasLocInfo = !!node.loc;
@@ -586,9 +603,6 @@ module.exports = class ParseResult {
         break;
       case 'ConcatStatement':
         {
-          let openQuote = nodeInfo.source[0];
-          let endQuote = nodeInfo.source[nodeInfo.source.length - 1];
-
           let partsSource = this.sourceForLoc({
             start: {
               line: original.loc.start.line,
@@ -607,7 +621,7 @@ module.exports = class ParseResult {
             dirtyFields.delete('parts');
           }
 
-          output.push(openQuote, partsSource, endQuote);
+          output.push(partsSource);
         }
         break;
       case 'BlockStatement':
@@ -847,28 +861,19 @@ module.exports = class ParseResult {
       case 'AttrNode':
         {
           let { source } = nodeInfo;
-          let [, nameSource, postNameWhitespace, equals, postEqualsWhitespace] = source.match(
-            attrNodeParts
-          );
+          let [
+            ,
+            nameSource,
+            postNameWhitespace,
+            equals,
+            postEqualsWhitespace,
+            quote,
+          ] = source.match(attrNodeParts);
           let valueSource = this.sourceForLoc(original.value.loc);
 
           // does not include ConcatStatement because `_print` automatically
           // adds a `"` around them, meaning we do not need to add our own quotes
           let wasQuotableValue = original.value.type === 'TextNode';
-
-          let openQuote = '';
-          let closeQuote = '';
-
-          if (
-            original.value.type === 'TextNode' &&
-            original.value.chars.length > 0 &&
-            ((valueSource.startsWith(`'`) && valueSource.endsWith(`'`)) ||
-              (valueSource.startsWith(`"`) && valueSource.endsWith(`"`)))
-          ) {
-            openQuote = valueSource[0];
-            closeQuote = valueSource[valueSource.length - 1];
-            valueSource = valueSource.slice(1, -1);
-          }
 
           if (dirtyFields.has('name')) {
             nameSource = ast.name;
@@ -880,11 +885,9 @@ module.exports = class ParseResult {
             let newValueNeedsQuotes = ast.value.type === 'TextNode';
 
             if (!wasQuotableValue && newValueNeedsQuotes) {
-              openQuote = '"';
-              closeQuote = '"';
+              quote = '"';
             } else if (wasQuotableValue && !newValueNeedsQuotes) {
-              openQuote = '';
-              closeQuote = '';
+              quote = '';
             }
 
             valueSource = this.print(ast.value);
@@ -897,9 +900,9 @@ module.exports = class ParseResult {
             postNameWhitespace,
             equals,
             postEqualsWhitespace,
-            openQuote,
+            quote,
             valueSource,
-            closeQuote
+            quote
           );
         }
         break;

commit f909cc5df5931260e155c98512d6fb2ac5231c30

Expose metadata (isValueless / quoteType) on AttrNode's
The fixes introduced in a589561e caused issues for
ember-template-lint@2.x branch (which uses ember-template-recast as its
parser). The underlying reason for the issue is that ember-template-lint
was _basically_ relying on the bug fixed (e.g. it was looking at
`AttrNode`'s `.value` and doing `sourceForLoc` on that node, then
checking if it starts with a quote) .

In order for those ember-template-lint rules to function (`quotes` and
`no-quoteless-attribute`) they need to know if a given attribute had a
value (aka was it `<div data-test-blah></div>` vs `<div
data-test-blah="thing"></div>`) and what type of quotes were used
(single, double, none, etc).

This adds to the existing `AttrNode` interface, such that it is:

```ts
export interface AttrNode extends BaseNode {
  type: 'AttrNode';
  name: string;
  value: TextNode | MustacheStatement | ConcatStatement;
  isValueless: boolean;
  quoteType: '"' | "'" | null;
}
```

src/parse-result.js
@@ -42,7 +42,8 @@ function fixASTIssues(sourceLines, ast) {
   traverse(ast, {
     AttrNode(node) {
       let source = sourceForLoc(sourceLines, node.loc);
-      let isValueless = !source.includes('=');
+      let [, , , equals, , quote] = source.match(attrNodeParts);
+      let isValueless = !equals;
 
       // TODO: manually working around https://github.com/glimmerjs/glimmer-vm/pull/953
       if (isValueless && node.value.type === 'TextNode' && node.value.chars === '') {
@@ -51,6 +52,9 @@ function fixASTIssues(sourceLines, ast) {
         node.loc.end.line = node.loc.start.line;
         node.loc.end.column = node.loc.start.column + node.name.length;
       }
+
+      node.isValueless = isValueless;
+      node.quoteType = quote ? quote : null;
     },
     TextNode(node, path) {
       let source = sourceForLoc(sourceLines, node.loc);

tests/parse-result-test.js
@@ -1200,6 +1200,88 @@ QUnit.module('ember-template-recast', function() {
       assert.equal(print(ast), '<Foo bar="{{foo}} static {{bar}}" />');
     });
 
+    QUnit.test(
+      'can determine if an AttrNode was valueless (required by ember-template-lint)',
+      function(assert) {
+        assert.strictEqual(
+          parse(`<Foo bar={{foo}} />`).body[0].attributes[0].isValueless,
+          false,
+          'MustacheStatement attribute value'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar="foo {{bar}}" />`).body[0].attributes[0].isValueless,
+          false,
+          'ConcatStatement attribute value'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar='foo {{bar}}' />`).body[0].attributes[0].isValueless,
+          false,
+          'ConcatStatement attribute value'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar="foo" />`).body[0].attributes[0].isValueless,
+          false,
+          'TextNode attribute value'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar='foo' />`).body[0].attributes[0].isValueless,
+          false,
+          'TextNode attribute value'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar=foo />`).body[0].attributes[0].isValueless,
+          false,
+          'TextNode attribute value'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar />`).body[0].attributes[0].isValueless,
+          true,
+          'valueless attribute'
+        );
+      }
+    );
+
+    QUnit.test(
+      'can determine type of quotes used from AST (required by ember-template-lint)',
+      function(assert) {
+        assert.strictEqual(
+          parse(`<Foo bar={{foo}} />`).body[0].attributes[0].quoteType,
+          null,
+          'mustache attribute values are `null`'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar="foo {{bar}}" />`).body[0].attributes[0].quoteType,
+          `"`,
+          'ConcatStatement attribute values show double quotes'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar='foo {{bar}}' />`).body[0].attributes[0].quoteType,
+          `'`,
+          'ConcatStatement attribute values show single quotes'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar="foo" />`).body[0].attributes[0].quoteType,
+          `"`,
+          'TextNode attribute values show double quotes'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar='foo' />`).body[0].attributes[0].quoteType,
+          `'`,
+          'TextNode attribute values show single quotes'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar=foo />`).body[0].attributes[0].quoteType,
+          null,
+          'TextNode attribute values for quoteless'
+        );
+        assert.strictEqual(
+          parse(`<Foo bar />`).body[0].attributes[0].quoteType,
+          null,
+          'valueless attribute'
+        );
+      }
+    );
+
     QUnit.test('renaming valueless attribute', function(assert) {
       let template = '<Foo data-bar />';
 